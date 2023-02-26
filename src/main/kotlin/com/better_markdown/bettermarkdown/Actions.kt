8c10a2638227cd6e3f425671c7d18b1d56e01158
Eric Moss <yeswolfprod@gmail.com>
2023-02-26 01:42:26 -0500

    CHANGE: TableContentsGenerator
    - Change the action to insert TableOfContents at cursor position instead of top of file.

--- src/main/kotlin/com/better_markdown/bettermarkdown/Actions.kt
+++ src/main/kotlin/com/better_markdown/bettermarkdown/Actions.kt
@@ -1,5 +1,6 @@
 package com.better_markdown.bettermarkdown
 
+import com.better_markdown.bettermarkdown.formatters.TableFormatter
 import com.better_markdown.bettermarkdown.generators.TableOfContentsGenerator
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
@@ -14,17 +15,27 @@ class TableOfContentsAction : AnAction() {
         val editor = e.getData(CommonDataKeys.EDITOR) ?: return
         val document = editor.document
         val file = e.getData(CommonDataKeys.VIRTUAL_FILE) ?: return
+        val generator = TableOfContentsGenerator()
+        val markdown = document.text
+        val toc = generator.generateTableOfContents(markdown)
+
+        // Get the start and end offsets of the current line
+        val lineStartOffset = editor.caretModel.visualLineStart
+        val lineEndOffset = editor.caretModel.visualLineEnd
+        val insertOffset = if (lineEndOffset > markdown.length) markdown.length else lineEndOffset
 
         if (!file.extension.equals("md", true)) {
             Messages.showMessageDialog(project, "Table of contents can only be generated for Markdown files", "Table of Contents", Messages.getWarningIcon())
             return
         }
 
-        val generator = TableOfContentsGenerator()
-        val toc = generator.generateTableOfContents(document.text)
+        // TODO: Decide whether add new line before or after the table of contents (or make it configurable)
+        val formattedToc = "\n$toc\n"
 
         val writeAction = WriteCommandAction.runWriteCommandAction(project) {
-            document.insertString(0, toc)
+
+            // Insert the table of contents at the start of the current line
+            document.insertString(lineStartOffset, formattedToc)
         }
 //        val writeAction = object : WriteCommandAction.Simple<Any>(project, "Generate Table of Contents") {
 //            override fun run() {
@@ -35,6 +46,30 @@ class TableOfContentsAction : AnAction() {
         writeAction.run {
             Messages.showMessageDialog(project, "Table of contents generated and inserted at the top of the file", "Table of Contents", Messages.getInformationIcon())
         }
+    }
+}
 
+class FormatTableAction: AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        val project = e.project ?: return
+        val editor = e.getData(CommonDataKeys.EDITOR) ?: return
+        val document = editor.document
+        val file = e.getData(CommonDataKeys.VIRTUAL_FILE) ?: return
+
+        if (!file.extension.equals("md", true)) {
+            Messages.showMessageDialog(project, "Table can only be formatted for Markdown files", "Format Table", Messages.getWarningIcon())
+            return
+        }
+
+        val tableFormatter = TableFormatter()
+        val formattedTable = tableFormatter.formatTable(document.text)
+
+        val writeAction = WriteCommandAction.runWriteCommandAction(project) {
+            document.setText(formattedTable)
+        }
+
+        writeAction.run {
+            Messages.showMessageDialog(project, "Table formatted", "Format Table", Messages.getInformationIcon())
+        }
     }
 }
