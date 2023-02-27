4d892c46edb902db95fdee2425b23f3d6d1ca956
techygrrrl <techygrrrl@proton.me>
2023-02-26 16:42:41 -0800

    add BetterMarkdownDefaults constants for default config

--- src/test/kotlin/com/better_markdown/bettermarkdown/utils/IntRangeUtilsKtTest.kt
+++ src/test/kotlin/com/better_markdown/bettermarkdown/utils/IntRangeUtilsKtTest.kt
@@ -1,5 +1,6 @@
 package com.better_markdown.bettermarkdown.utils
 
+import com.better_markdown.bettermarkdown.settings.BetterMarkdownDefaults.DEFAULT_INT_RANGE_VALUE
 import org.junit.Test
 import org.junit.jupiter.api.Assertions.*
 
@@ -20,5 +21,10 @@ internal class IntRangeUtilsKtTest {
     @Test
     fun `returns null when invalid range provided with range syntax`() {
         assertNull("hello..shuttle".toIntRange())
+    }
+
+    @Test
+    fun `can toString() an IntRange`() {
+        assertEquals("1..6", DEFAULT_INT_RANGE_VALUE.toString())
     }
 }
