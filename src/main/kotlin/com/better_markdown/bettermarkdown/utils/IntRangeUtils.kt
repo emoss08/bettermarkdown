d57d244b36bfb8e10e2f46aac4294bbc691a2779
techygrrrl <techygrrrl@proton.me>
2023-02-26 17:31:22 -0800

    added the ability for the user to configure the range

--- src/main/kotlin/com/better_markdown/bettermarkdown/utils/IntRangeUtils.kt
+++ src/main/kotlin/com/better_markdown/bettermarkdown/utils/IntRangeUtils.kt
@@ -3,18 +3,13 @@ package com.better_markdown.bettermarkdown.utils
 fun String.toIntRange(): IntRange? {
     val split = this.split("..")
 
-    if (split.size != 2) {
-        println("Invalid Range")
-        return null
-    }
+    if (split.size != 2) return null
 
     val (a, b) = split
 
     return try {
         a.toInt()..b.toInt()
     } catch (e: NumberFormatException) {
-        e.printStackTrace()
-        println("Values '$a' and/or '$b' are not integers")
         null
     }
 }
